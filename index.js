9aef12175443e15a91b6a6d86ccaf4e2721bdd03
Rohit Yadav <131008606+rohit-yadav-4099@users.noreply.github.com>
2023-11-10 21:34:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const http = require("http");
 const jsondata = require('./jsonPackege')
 const server = http.createServer((request, response) => {
     if(request.url === '/'){
-        response.write("<h1>This is my HTTP home server</>")
+        response.write("<h1>What is Node js?</h1> <p>Node.js is an open-source, cross-platform JavaScript runtime environment and library for running web applications outside the client's browser. Ryan Dahl developed it in 2009.</p>")
+        response.write("<h1>Advantage of Node.js -</h1><p>Node js is faster than java because it uses an asynchronous event-driven I/O model and does not have the overhead of the Java runtime environment.</p>")
+        response.write("<h1>Benefits of Node.js- </h1><ol><li>High Performance.</li><li>Scalability</li><li>Easy to Learn</li></ol>")
         response.end()
     }else if(request.url === '/product'){
         const viewdata = JSON.stringify(jsondata);
